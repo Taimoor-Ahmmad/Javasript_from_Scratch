c7031282b80fbfe4d3a87d4d34ed263cadd49d15
Taimoor <taimoorahmad890@gmail.com>
2024-04-10 10:21:32 +0500

    while and do while loop

---
+++ 04-Iterations/two.js
@@ -0,0 +1,38 @@
+
+//while loop
+let index=0
+while(index<=10)
+{
+    console.log(`value of index is ${index}`)
+    index=index+2;
+}
+
+let myArr=['flash','batman','superman']
+
+let arr=0
+while(arr<myArr.length)
+{
+    console.log(`Value is ${myArr[arr]}`)
+    arr=arr+1
+}
+
+//do while loop
+let score=1
+do
+{
+    console.log(`Score is ${score}`);
+    score=score+1;
+
+}
+while(score<=10)
+
+
+//Interesting is 11
+let score1=11
+do
+{
+    console.log(`Score is ${score}`);
+    score=score+1;
+
+}
+while(score<=10)//Score is 11
