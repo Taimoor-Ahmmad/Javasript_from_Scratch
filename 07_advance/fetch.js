8b24b3e72de0ca38e24656bc48fa8360c53a079e
Taimoor <taimoorahmad890@gmail.com>
2024-04-11 20:57:35 +0500

    fetch method in js

---
+++ 07_advance/fetch.js
@@ -0,0 +1,5 @@
+/**the global fetch() method starts the process of fetching 
+ * a resource from the network---->network fetching method
+ * 
+ * HTTP errors are found in response rather than resolve
+ */
